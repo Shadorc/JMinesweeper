c374fff211ccc5ae5b6ec06f14f91610efe463c6
David <bi-crosseur@hotmail.fr>
2021-05-26 22:47:24 +0200

    Format code

--- src/me/shadorc/demineur/frame/Frame.java
+++ src/me/shadorc/demineur/frame/Frame.java
@@ -1,11 +1,8 @@
 package me.shadorc.demineur.frame;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.FontFormatException;
-import java.awt.GridLayout;
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -16,194 +13,183 @@ import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Locale;
 
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
-import javax.swing.Timer;
-import javax.swing.border.Border;
-
 class Frame extends JFrame implements ActionListener {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private Grid grid;
+    private Grid grid;
 
-	private Border border;
-	private Font font;
+    private Border border;
+    private Font font;
 
-	private static JLabel bombsInfo = new JLabel("", JLabel.CENTER);
-	private static JLabel timeInfo = new JLabel("", JLabel.CENTER);
+    private static JLabel bombsInfo = new JLabel("", JLabel.CENTER);
+    private static JLabel timeInfo = new JLabel("", JLabel.CENTER);
 
-	private JButton scoresButton = new JButton("Scores");
-	private JButton gameButton = new JButton("Nouvelle Partie");
+    private JButton scoresButton = new JButton("Scores");
+    private JButton gameButton = new JButton("Nouvelle Partie");
 
-	private static DecimalFormat df;
-	private static float time = 0;
+    private static DecimalFormat df;
+    private static float time = 0;
 
-	private static Timer timer = new Timer(100, new ActionListener() {
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			time += 0.1;
-			Frame.refresh();
-		}
-	});
+    private static Timer timer = new Timer(100, new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent arg0) {
+            time += 0.1;
+            Frame.refresh();
+        }
+    });
 
-	public static void main(String[] args) {
-		// Change the separator "," to "."
-		df = (DecimalFormat) NumberFormat.getNumberInstance(new Locale("en", "UK"));
-		df.applyPattern("0.0");
+    public static void main(String[] args) {
+        // Change the separator "," to "."
+        df = (DecimalFormat) NumberFormat.getNumberInstance(new Locale("en", "UK"));
+        df.applyPattern("0.0");
 
-		Scores.read();
+        Scores.read();
 
-		SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				new Frame();
-			}
-		});
-	}
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Frame();
+            }
+        });
+    }
 
-	Frame() {
+    Frame() {
 
-		JFrame frame = new JFrame("Démineur by Shadorc");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setBackground(new Color(213, 221, 232));
+        JFrame frame = new JFrame("Démineur by Shadorc");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBackground(new Color(213, 221, 232));
 
-		JPanel contenuFenetre = new JPanel(new BorderLayout());
-		contenuFenetre.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-		contenuFenetre.setOpaque(false);
+        JPanel contenuFenetre = new JPanel(new BorderLayout());
+        contenuFenetre.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        contenuFenetre.setOpaque(false);
 
-		frame.setContentPane(contenuFenetre);
+        frame.setContentPane(contenuFenetre);
 
-		JPanel hautFenetre = new JPanel(new GridLayout(2, 0, 0, 5));
-		hautFenetre.setBorder(BorderFactory.createEmptyBorder(0, 3, 3, 3));
-		hautFenetre.setOpaque(false);
+        JPanel hautFenetre = new JPanel(new GridLayout(2, 0, 0, 5));
+        hautFenetre.setBorder(BorderFactory.createEmptyBorder(0, 3, 3, 3));
+        hautFenetre.setOpaque(false);
 
-		border = BorderFactory.createLineBorder(new Color(0, 100, 255), 3, true);
-		try {
-			font = Font.createFont(Font.TRUETYPE_FONT, this.getClass().getResourceAsStream("/res/ASENINE.TTF")).deriveFont(Font.BOLD, 30);
-		} catch (FontFormatException | IOException e1) {
-			font = new Font("Serif", Font.BOLD, 30);
-		}
+        border = BorderFactory.createLineBorder(new Color(0, 100, 255), 3, true);
+        try {
+            font = Font.createFont(Font.TRUETYPE_FONT, this.getClass().getResourceAsStream("/res/ASENINE.TTF")).deriveFont(Font.BOLD, 30);
+        } catch (FontFormatException | IOException e1) {
+            font = new Font("Serif", Font.BOLD, 30);
+        }
 
-		this.config(scoresButton);
-		this.config(gameButton);
+        this.config(scoresButton);
+        this.config(gameButton);
 
-		hautFenetre.add(scoresButton);
-		hautFenetre.add(gameButton);
+        hautFenetre.add(scoresButton);
+        hautFenetre.add(gameButton);
 
-		contenuFenetre.add(hautFenetre, BorderLayout.NORTH);
+        contenuFenetre.add(hautFenetre, BorderLayout.NORTH);
 
-		grid = new Grid(9, 9);
-		grid.setBorder(BorderFactory.createEmptyBorder(6, 3, 6, 3));
-		grid.setOpaque(false);
-		contenuFenetre.add(grid, BorderLayout.CENTER);
+        grid = new Grid(9, 9);
+        grid.setBorder(BorderFactory.createEmptyBorder(6, 3, 6, 3));
+        grid.setOpaque(false);
+        contenuFenetre.add(grid, BorderLayout.CENTER);
 
-		JPanel infosPane = new JPanel(new GridLayout(0, 2, 10, 0));
-		infosPane.setBorder(BorderFactory.createEmptyBorder(3, 3, 0, 3));
-		infosPane.setOpaque(false);
+        JPanel infosPane = new JPanel(new GridLayout(0, 2, 10, 0));
+        infosPane.setBorder(BorderFactory.createEmptyBorder(3, 3, 0, 3));
+        infosPane.setOpaque(false);
 
-		this.config(bombsInfo, "/res/bombe.png");
-		this.config(timeInfo, "/res/time.png");
+        this.config(bombsInfo, "/res/bombe.png");
+        this.config(timeInfo, "/res/time.png");
 
-		infosPane.add(bombsInfo);
-		infosPane.add(timeInfo);
+        infosPane.add(bombsInfo);
+        infosPane.add(timeInfo);
 
-		contenuFenetre.add(infosPane, BorderLayout.SOUTH);
+        contenuFenetre.add(infosPane, BorderLayout.SOUTH);
 
-		frame.setIconImage(new ImageIcon(this.getClass().getResource("/res/bombe.png")).getImage());
-		frame.pack();
-		frame.setSize(new Dimension(600, 770));
-		frame.setMinimumSize(new Dimension(520, 685));
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
-	}
+        frame.setIconImage(new ImageIcon(this.getClass().getResource("/res/bombe.png")).getImage());
+        frame.pack();
+        frame.setSize(new Dimension(600, 770));
+        frame.setMinimumSize(new Dimension(520, 685));
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 
-	private void config(JButton button) {
-		button.setFont(font);
-		button.setBorder(border);
-		button.setBackground(Color.WHITE);
-		button.addActionListener(this);
-		button.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				((JButton) e.getSource()).setBackground(new Color(220, 220, 220));
-			}
+    private void config(JButton button) {
+        button.setFont(font);
+        button.setBorder(border);
+        button.setBackground(Color.WHITE);
+        button.addActionListener(this);
+        button.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                ((JButton) e.getSource()).setBackground(new Color(220, 220, 220));
+            }
 
-			@Override
-			public void mouseExited(MouseEvent e) {
-				((JButton) e.getSource()).setBackground(Color.WHITE);
-			}
-		});
-	}
+            @Override
+            public void mouseExited(MouseEvent e) {
+                ((JButton) e.getSource()).setBackground(Color.WHITE);
+            }
+        });
+    }
 
-	private void config(JLabel label, String path) {
-		label.setFont(new Font("ARIAL", Font.PLAIN, 40));
-		label.setBorder(border);
-		label.setOpaque(true);
-		label.setForeground(Color.BLACK);
-		label.setBackground(Color.WHITE);
-		label.setIcon(new ImageIcon(this.getClass().getResource(path)));
-	}
+    private void config(JLabel label, String path) {
+        label.setFont(new Font("ARIAL", Font.PLAIN, 40));
+        label.setBorder(border);
+        label.setOpaque(true);
+        label.setForeground(Color.BLACK);
+        label.setBackground(Color.WHITE);
+        label.setIcon(new ImageIcon(this.getClass().getResource(path)));
+    }
 
-	protected static void start() {
-		timer.start();
-	}
+    protected static void start() {
+        timer.start();
+    }
 
-	protected static void stop(boolean victoire) {
-		timer.stop();
+    protected static void stop(boolean victoire) {
+        timer.stop();
 
-		if(victoire) {
-			String record = "";
-			if(Scores.getScore().size() == 0 || Scores.getScore().get(0) > time) {
-				record = "\n! NOUVEAU RECORD !";
-			}
+        if (victoire) {
+            String record = "";
+            if (Scores.getScore().size() == 0 || Scores.getScore().get(0) > time) {
+                record = "\n! NOUVEAU RECORD !";
+            }
 
-			JOptionPane.showConfirmDialog(null, "Partie terminée. Temps : " + df.format(time) + "s." + record, "Partie finie", JOptionPane.PLAIN_MESSAGE);
-			Scores.add(df.format(time));
+            JOptionPane.showConfirmDialog(null, "Partie terminée. Temps : " + df.format(time) + "s." + record, "Partie finie", JOptionPane.PLAIN_MESSAGE);
+            Scores.add(df.format(time));
 
-		} else {
-			JOptionPane.showConfirmDialog(null, "Partie terminée. Vous avez perdu.", "Partie finie", JOptionPane.PLAIN_MESSAGE);
-		}
-	}
+        } else {
+            JOptionPane.showConfirmDialog(null, "Partie terminée. Vous avez perdu.", "Partie finie", JOptionPane.PLAIN_MESSAGE);
+        }
+    }
 
-	protected static void reset() {
-		timer.stop();
-		time = 0;
-		Frame.refresh();
-	}
+    protected static void reset() {
+        timer.stop();
+        time = 0;
+        Frame.refresh();
+    }
 
-	protected static void refresh() {
-		bombsInfo.setText(Integer.toString(Grid.foundBombs));
-		timeInfo.setText(df.format(time));
-	}
+    protected static void refresh() {
+        bombsInfo.setText(Integer.toString(Grid.foundBombs));
+        timeInfo.setText(df.format(time));
+    }
 
-	@Override
-	public void actionPerformed(ActionEvent event) {
-		JButton button = (JButton) event.getSource();
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        JButton button = (JButton) event.getSource();
 
-		if(button.equals(gameButton)) {
-			grid.init(false);
+        if (button.equals(gameButton)) {
+            grid.init(false);
 
-		} else if(button.equals(scoresButton)) {
-			String text = "Scores :\n";
+        } else if (button.equals(scoresButton)) {
+            String text = "Scores :\n";
 
-			ArrayList <Float> scores = Scores.getScore();
+            ArrayList<Float> scores = Scores.getScore();
 
-			for(int i = 0; i < scores.size() && i < 5; i++) {
-				text += "\n" + (i + 1) + " : " + scores.get(i) + "s";
-			}
+            for (int i = 0; i < scores.size() && i < 5; i++) {
+                text += "\n" + (i + 1) + " : " + scores.get(i) + "s";
+            }
 
-			if(scores.size() > 0) {
-				JOptionPane.showMessageDialog(null, text, "Scores", JOptionPane.PLAIN_MESSAGE);
-			} else {
-				JOptionPane.showMessageDialog(null, "Aucun score enregistré.", "Scores", JOptionPane.PLAIN_MESSAGE);
-			}
-		}
-	}
+            if (scores.size() > 0) {
+                JOptionPane.showMessageDialog(null, text, "Scores", JOptionPane.PLAIN_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(null, "Aucun score enregistré.", "Scores", JOptionPane.PLAIN_MESSAGE);
+            }
+        }
+    }
 }
--- src/me/shadorc/demineur/frame/Grid.java
+++ src/me/shadorc/demineur/frame/Grid.java
@@ -1,168 +1,165 @@
 package me.shadorc.demineur.frame;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.GridLayout;
+import me.shadorc.demineur.frame.Case.State;
+
+import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Random;
 
-import javax.swing.JPanel;
-
-import me.shadorc.demineur.frame.Case.State;
-
 class Grid extends JPanel {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private Case cases[][];
-	private Random rand = new Random();
+    private Case cases[][];
+    private Random rand = new Random();
 
-	protected static int foundBombs;
-	private int remainingCompartments;
-	private int width, height;
+    protected static int foundBombs;
+    private int remainingCompartments;
+    private int width, height;
 
-	private boolean bombesPosees;
+    private boolean bombesPosees;
 
-	Grid(int width, int height) {
-		super(new GridLayout(width, height));
+    Grid(int width, int height) {
+        super(new GridLayout(width, height));
 
-		this.width = width;
-		this.height = height;
+        this.width = width;
+        this.height = height;
 
-		cases = new Case[width][height];
+        cases = new Case[width][height];
 
-		this.init(true);
+        this.init(true);
 
-		Case.grid = this;
-	}
+        Case.grid = this;
+    }
 
-	protected void init(boolean firstTime) {
+    protected void init(boolean firstTime) {
 
-		bombesPosees = false;
-		foundBombs = 10;
-		remainingCompartments = width * height - foundBombs;
+        bombesPosees = false;
+        foundBombs = 10;
+        remainingCompartments = width * height - foundBombs;
 
-		// Positioning buttons in the grid.
-		for(int x = 0; x < width; x++) {
-			for(int y = 0; y < height; y++) {
-				if(firstTime) {
-					this.add(cases[x][y] = new Case(x, y));
-				} else {
-					cases[x][y].reset();
-				}
+        // Positioning buttons in the grid.
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                if (firstTime) {
+                    this.add(cases[x][y] = new Case(x, y));
+                } else {
+                    cases[x][y].reset();
+                }
 
-				// Background color blue
-				cases[x][y].setBackground(new Color(0, 100, 255));
-			}
-		}
+                // Background color blue
+                cases[x][y].setBackground(new Color(0, 100, 255));
+            }
+        }
 
-		Frame.reset();
-	}
+        Frame.reset();
+    }
 
-	protected void placeBombs(Case c) {
+    protected void placeBombs(Case c) {
 
-		int bombes = 0;
-		bombesPosees = true;
+        int bombes = 0;
+        bombesPosees = true;
 
-		// Positioning bombs in the grid.
-		while(bombes < foundBombs) {
+        // Positioning bombs in the grid.
+        while (bombes < foundBombs) {
 
-			Case randCase = cases[rand.nextInt(width)][rand.nextInt(height)];
+            Case randCase = cases[rand.nextInt(width)][rand.nextInt(height)];
 
-			// If the case don't already contain a bomb & isn't reveal.
-			if(!randCase.isBomb() && randCase != c && !this.getAround(randCase).contains(c)) {
-				randCase.setBomb();
-				bombes++;
+            // If the case don't already contain a bomb & isn't reveal.
+            if (!randCase.isBomb() && randCase != c && !this.getAround(randCase).contains(c)) {
+                randCase.setBomb();
+                bombes++;
 
-				for(Case around : this.getAround(randCase)) {
-					around.increment();
-				}
-			}
-		}
-	}
+                for (Case around : this.getAround(randCase)) {
+                    around.increment();
+                }
+            }
+        }
+    }
 
-	private ArrayList <Case> getAround(Case c) {
+    private ArrayList<Case> getAround(Case c) {
 
-		ArrayList <Case> around = new ArrayList <Case>();
+        ArrayList<Case> around = new ArrayList<Case>();
 
-		int x = c.getCoX();
-		int y = c.getCoY();
+        int x = c.getCoX();
+        int y = c.getCoY();
 
-		if(this.inBounds(x + 1, y)) {
-			around.add(cases[x + 1][y]);
-		}
-		if(this.inBounds(x - 1, y)) {
-			around.add(cases[x - 1][y]);
-		}
-		if(this.inBounds(x, y + 1)) {
-			around.add(cases[x][y + 1]);
-		}
-		if(this.inBounds(x, y - 1)) {
-			around.add(cases[x][y - 1]);
-		}
-		if(this.inBounds(x + 1, y + 1)) {
-			around.add(cases[x + 1][y + 1]);
-		}
-		if(this.inBounds(x - 1, y - 1)) {
-			around.add(cases[x - 1][y - 1]);
-		}
-		if(this.inBounds(x + 1, y - 1)) {
-			around.add(cases[x + 1][y - 1]);
-		}
-		if(this.inBounds(x - 1, y + 1)) {
-			around.add(cases[x - 1][y + 1]);
-		}
+        if (this.inBounds(x + 1, y)) {
+            around.add(cases[x + 1][y]);
+        }
+        if (this.inBounds(x - 1, y)) {
+            around.add(cases[x - 1][y]);
+        }
+        if (this.inBounds(x, y + 1)) {
+            around.add(cases[x][y + 1]);
+        }
+        if (this.inBounds(x, y - 1)) {
+            around.add(cases[x][y - 1]);
+        }
+        if (this.inBounds(x + 1, y + 1)) {
+            around.add(cases[x + 1][y + 1]);
+        }
+        if (this.inBounds(x - 1, y - 1)) {
+            around.add(cases[x - 1][y - 1]);
+        }
+        if (this.inBounds(x + 1, y - 1)) {
+            around.add(cases[x + 1][y - 1]);
+        }
+        if (this.inBounds(x - 1, y + 1)) {
+            around.add(cases[x - 1][y + 1]);
+        }
 
-		return around;
-	}
+        return around;
+    }
 
-	private boolean inBounds(int x, int y) {
-		return (x >= 0) && (x < 9) && (y >= 0) && (y < 9);
-	}
+    private boolean inBounds(int x, int y) {
+        return (x >= 0) && (x < 9) && (y >= 0) && (y < 9);
+    }
 
-	protected void check(int x, int y) {
-		if(this.inBounds(x, y)) {
+    protected void check(int x, int y) {
+        if (this.inBounds(x, y)) {
 
-			Case c = cases[x][y];
+            Case c = cases[x][y];
 
-			if(c.getState() == State.DEFAULT || c.getState() == State.INTERROGATION) {
-				remainingCompartments--;
-				c.reveal();
+            if (c.getState() == State.DEFAULT || c.getState() == State.INTERROGATION) {
+                remainingCompartments--;
+                c.reveal();
 
-				if(c.getBombsAround() == 0) {
-					for(Case ca : this.getAround(c)) {
-						this.check(ca.getCoX(), ca.getCoY());
-					}
-				}
-			}
-		}
-	}
+                if (c.getBombsAround() == 0) {
+                    for (Case ca : this.getAround(c)) {
+                        this.check(ca.getCoX(), ca.getCoY());
+                    }
+                }
+            }
+        }
+    }
 
-	protected void endGame(boolean win) {
+    protected void endGame(boolean win) {
 
-		Frame.stop(win);
+        Frame.stop(win);
 
-		// Deactivate all buttons.
-		for(int i = 0; i < width; i++) {
-			for(int o = 0; o < height; o++) {
-				cases[i][o].disable();
-			}
-		}
-	}
+        // Deactivate all buttons.
+        for (int i = 0; i < width; i++) {
+            for (int o = 0; o < height; o++) {
+                cases[i][o].disable();
+            }
+        }
+    }
 
-	public Case[][] getGrid() {
-		return cases;
-	}
+    public Case[][] getGrid() {
+        return cases;
+    }
 
-	public Dimension getDimension() {
-		return new Dimension(width, height);
-	}
+    public Dimension getDimension() {
+        return new Dimension(width, height);
+    }
 
-	public int getRemainingCompartments() {
-		return remainingCompartments;
-	}
+    public int getRemainingCompartments() {
+        return remainingCompartments;
+    }
 
-	protected boolean bombsPlaced() {
-		return bombesPosees;
-	}
+    protected boolean bombsPlaced() {
+        return bombesPosees;
+    }
 }
